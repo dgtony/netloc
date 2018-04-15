48661d257acf87d6334c2e93f954729c05f5251e
Anton Dort-Golts <anton.dort-golts@iqoption.com>
2018-04-16 00:00:14 +0300

    Add: landmark agent

--- src/bin/bootstrap.rs
+++ src/bin/bootstrap.rs
@@ -1,3 +1,9 @@
+//! Bootstrap server.
+//!
+//! Mandatory part of network location infrastructure.
+//! Bootstrap server allows recently connected agents
+//! to find its first neighbours to start communication.
+//!
 
 extern crate netloc;
 
@@ -6,4 +12,9 @@ use netloc::agent::bootstrap;
 
 fn main() {
     println!("bootstrap server not implemented!")
+
+    // todo:
+    // - use hardcoded address of landmark-agent with zero coordinates
+    // - add it into the storage
+    // - run UDP-server
 }
---
+++ src/bin/landmark.rs
@@ -0,0 +1,14 @@
+//! Landmark agent
+//!
+//! Simple agent that has hardcoded zero coordinates and never
+//! recalculate it. This agent could be used as an anchor
+//! for drifting network coordinates.
+//!
+
+extern crate netloc;
+
+use netloc::agent;
+
+fn main() {
+    println!("Landmark agent not implemented!")
+}
