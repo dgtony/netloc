49e70dd93cb2127e4c889b77c475ea45bfaeac24
Anton Dort-Golts <anton.dort-golts@iqoption.com>
2018-04-23 01:05:53 +0300

    Use logger in landmark agent

--- src/bin/landmark.rs
+++ src/bin/landmark.rs
@@ -6,6 +6,9 @@
 //!
 
 extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate loggerv;
 extern crate netloc;
 
 use std::net::IpAddr;
@@ -31,46 +34,88 @@ fn parse_args(agent_addr: IpAddr, agent_port: u16) -> agent::AgentConfig {
     config
 }
 
+fn parse_log_level(level: &str) -> Option<log::Level> {
+    match level {
+        "debug" => Some(log::Level::Debug),
+        "info" => Some(log::Level::Info),
+        "warn" => Some(log::Level::Warn),
+        "error" => Some(log::Level::Error),
+        _ => None,
+    }
+}
+
 fn main() {
-    let args = clap::App::new("netloc-landmark")
+    let args = App::new("netloc-landmark")
         .version("0.1")
         .author("Anton Dort-Golts dortgolts@gmail.com")
         .about("Landmark agent for the Vivaldi network coordinate system")
         .arg(
-            clap::Arg::with_name("addr")
+            Arg::with_name("addr")
                 .short("a")
                 .long("addr")
+                .value_name("IP-address")
                 .help("IP-address to listen on")
                 .takes_value(true)
                 .default_value("0.0.0.0"),
         )
         .arg(
-            clap::Arg::with_name("port")
+            Arg::with_name("port")
                 .short("p")
                 .long("port")
+                .value_name("UDP-port")
                 .help("UDP-port to listen on")
                 .takes_value(true)
                 .default_value("3738"),
+        )
+        .arg(
+            Arg::with_name("log_level")
+                .short("l")
+                .long("log")
+                .value_name("level")
+                .help("logging level")
+                .takes_value(true)
+                .possible_values(&["debug", "info", "warn", "error"])
+                .default_value("info"),
         )
         .get_matches();
 
     let agent_addr = args.value_of("addr").and_then(|a| IpAddr::from_str(a).ok());
     let agent_port = args.value_of("port").unwrap().parse::<u16>();
+    let log_level = args.value_of("log_level")
+        .and_then(|l| parse_log_level(l))
+        .unwrap();
+
+    // init logger
+    loggerv::Logger::new()
+        .max_level(log_level)
+        .level(true)
+        .separator(" | ")
+        .colors(true)
+        .no_module_path()
+        .init()
+        .unwrap();
 
     // validation
     if agent_addr.is_none() {
-        println!("ERROR | bad IP address: {}", args.value_of("addr").unwrap());
+        error!("bad IP address: {}", args.value_of("addr").unwrap());
         process::exit(1);
     }
     if let Err(_) = agent_port {
-        println!("ERROR | bad port value: {}", args.value_of("port").unwrap());
+        error!("bad port value: {}", args.value_of("port").unwrap());
+        process::exit(1);
+    }
+    if agent_addr.is_none() {
+        error!("bad IP address: {}", args.value_of("addr").unwrap());
         process::exit(1);
     }
 
-    let config = parse_args(agent_addr.unwrap(), agent_port.unwrap());
-    println!("INFO | starting landmark agent");
+    let agent_addr = agent_addr.unwrap();
+    let agent_port = agent_port.unwrap();
+
+    let config = parse_args(agent_addr, agent_port);
+    info!("landmark agent started at {}:{}", agent_addr, agent_port);
 
     if let Err(e) = agent::run_landmark_agent(&config) {
-        println!("ERROR | agent failure: {}", e);
+        error!("agent failure: {}", e);
     }
 }
