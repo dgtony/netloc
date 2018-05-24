f8ffb51b4e42036ef9c90dd7ae76e79a54ee8bb8
Anton Dort-Golts <dortgolts@gmail.com>
2018-05-24 13:31:37 +0300

    Enable tokio

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,12 @@ extern crate log;
 extern crate loggerv;
 extern crate rand;
 
+#[macro_use]
+extern crate futures;
+extern crate tokio;
+extern crate tokio_io;
+extern crate bytes;
+
 pub mod agent;
 pub mod interface;
 pub mod storage;
